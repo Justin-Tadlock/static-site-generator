2b381b1d497a8492ac593d707d694d01839d2fa1
Justin Tadlock <8680592+Justin-Tadlock@users.noreply.github.com>
2025-01-26 23:30:59 -0700

    Adding ParentNode and some basic tests

---
+++ src/parentnode.py
@@ -0,0 +1,23 @@
+from htmlnode import HTMLNode
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag: str, children: list, props: dict=None):
+        super().__init__(tag, None, children, props)
+        self.tag = tag
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        if self.tag is None:
+            raise ValueError("ParentNode tag cannot be None")
+        if type(self.children) is not list:
+            raise ValueError("ParentNode children must be a list of HTMLNodes")
+        if self.children is None:
+            raise ValueError("ParentNode children cannot be None")
+        
+        content = ""
+        if len(self.children) > 0:
+            for c in self.children:
+                content += c.to_html()
+
+        return f'<{self.tag}{self.props_to_html()}>{content}</{self.tag}>'
---
+++ src/test_parentnode.py
@@ -0,0 +1,36 @@
+import unittest
+
+from htmlnode import HTMLNode
+from leafnode import LeafNode
+from parentnode import ParentNode
+from textnode import TextNode, TextType
+
+class TestParentNode(unittest.TestCase):
+    def test_to_html(self):
+        node = ParentNode("div", [])
+        self.assertEqual(node.to_html(), "<div></div>")
+
+    def test_to_html_plain_text(self):
+        node = ParentNode("div", [LeafNode(value="Hello world")])
+        self.assertEqual(node.to_html(), "<div>Hello world</div>")
+    
+    def test_to_html_with_p_leaf(self):
+        node = ParentNode("div", [LeafNode(tag="p", value="Hello world")])
+        self.assertEqual(node.to_html(), "<div><p>Hello world</p></div>")
+    
+    def test_to_html_with_p_and_b_leafnodes(self):
+        node = ParentNode("div", [
+            LeafNode(tag="p", value="Hello world"),
+            LeafNode(tag="b", value="Hello BOLD world")
+        ])
+        self.assertEqual(node.to_html(), "<div><p>Hello world</p><b>Hello BOLD world</b></div>")
+    
+    def test_to_html_with_nested_parentnode(self):
+        node = ParentNode("div", [
+            ParentNode(tag="p", children=[
+                LeafNode(tag=None, value="Hello world"),
+                LeafNode(tag="b", value="Hello BOLD world"),
+                LeafNode(tag=None, value="Hello plain world")
+            ])
+        ])
+        self.assertEqual(node.to_html(), "<div><p>Hello world<b>Hello BOLD world</b>Hello plain world</p></div>")
