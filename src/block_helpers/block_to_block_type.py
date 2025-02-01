0ef8111d7620ae88bd6b8bcadd1c787cc4153f5e
Justin Tadlock <8680592+Justin-Tadlock@users.noreply.github.com>
2025-01-31 23:36:39 -0700

    Refining the block type function

--- src/block_helpers/block_to_block_type.py
+++ src/block_helpers/block_to_block_type.py
@@ -1,18 +1,42 @@
 import re
+from enum import Enum
 
-def block_to_block_type(block_text):
-    headings = ['# ', '## ', '### ', '#### ', '##### ', '##### ', '###### ']
+class BlockType(Enum):
+    HEADING = 'heading'
+    CODE = 'code'
+    QUOTE = 'quote'
+    U_LIST = 'unordered_list'
+    O_LIST = 'ordered_list'
+    PARAGRAPH = 'paragraph'
 
-    for h in headings:
-        if block_text.startswith(h):
-            return 'Heading'
-    if block_text.startswith('```'):
-        return 'Code'
-    if block_text.startswith('> '):
-        return 'Quote'
-    if block_text.startswith('* ') or block_text.startswith('- '):
-        return 'Unordered List'
-    if re.match(r"(\d+\.\ )", block_text) is not None:
-        return 'Ordered List'
-    return 'Paragraph'
+def block_to_block_type(block):
+    lines = block.split('\n')
+
+    if block.startswith(('# ', '## ', '### ', '#### ', '##### ', '##### ', '###### ')):
+        return BlockType.HEADING
+    if len(lines) > 0 and lines[0].startswith('```') and lines[-1].startswith('```'):
+        return BlockType.CODE
+    if block.startswith('> '):
+        for line in lines:
+            if not line.startswith('>'):
+                return BlockType.PARAGRAPH
+        return BlockType.QUOTE
+    if block.startswith('* '):
+        for line in lines:
+            if not line.startswith('* '):
+                return BlockType.PARAGRAPH
+        return BlockType.U_LIST
+    if block.startswith('- '):
+        for line in lines:
+            if not line.startswith('- '):
+                return BlockType.PARAGRAPH
+        return BlockType.U_LIST
+    if block.startswith('1. '):
+        i = 1
+        for line in lines:
+            if not line.startswith(f"{i}. "):
+                return BlockType.PARAGRAPH
+            i += 1
+        return BlockType.O_LIST
+    return BlockType.PARAGRAPH
     
--- src/block_helpers/test_block_to_block_type.py
+++ src/block_helpers/test_block_to_block_type.py
@@ -1,34 +1,41 @@
 import unittest
 
-from .block_to_block_type import block_to_block_type
+from .block_to_block_type import BlockType, block_to_block_type
 
 class TestBlockToBlock(unittest.TestCase):
     def test_heading(self):
         for i in range(1,7):
             heading = f"{'#' * i} Heading"
             result = block_to_block_type(heading)
-            self.assertEqual(result, 'Heading')
+            self.assertEqual(result, BlockType.HEADING)
     
     def test_code(self):
         result = block_to_block_type('```code block here```')
-        self.assertEqual(result, 'Code')
+        self.assertEqual(result, BlockType.CODE)
+
+        result = block_to_block_type('```\ncode block here\n```')
+        self.assertEqual(result, BlockType.CODE)
     
     def test_quote(self):
         result = block_to_block_type('> Quote block here')
-        self.assertEqual(result, 'Quote')
+        self.assertEqual(result, BlockType.QUOTE)
     
     def test_unordered_list(self):
         result = block_to_block_type('* list items')
-        self.assertEqual(result, 'Unordered List')
+        self.assertEqual(result, BlockType.U_LIST)
 
         result = block_to_block_type('- list items')
-        self.assertEqual(result, 'Unordered List')
+        self.assertEqual(result, BlockType.U_LIST)
     
     def test_ordered_list(self):
-        for i in range(99):
-            result = block_to_block_type(f"{i}. List Items")
-            self.assertEqual(result, 'Ordered List')
+        block_text = ''
+        for i in range(1,10):
+            block_text += f"{i}. List Item\n"
+        block_text = block_text.rstrip('\n')
+
+        result = block_to_block_type(block_text)
+        self.assertEqual(result, BlockType.O_LIST)
     
     def test_paragraph(self):
         result = block_to_block_type('Lots of text here')
-        self.assertEqual(result, 'Paragraph')
+        self.assertEqual(result, BlockType.PARAGRAPH)
